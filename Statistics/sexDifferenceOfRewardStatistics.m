a930e0d1abdb9cd45518006175fa51d4f56665a9
atanugiri <agiri@miners.utep.edu>
2023-08-09 15:14:08 -0600

    Script for gender difference at different reward

--- Statistics/sexDifferenceOfRewardStatistics.m
+++ Statistics/sexDifferenceOfRewardStatistics.m
@@ -5,9 +5,11 @@
 % and for BL vs FD plots. It also performs KS2 and ranksum test for sex
 % difference at each concentration value.
 
-function sexDifferenceStatistics
+function sexDifferenceOfRewardStatistics
 clear; close all; clc;
-loadFile = load("BLsexDiffApproachPassing.mat"); % Load the desired mat file for analysis
+
+% Load the desired mat file for analysis
+loadFile = load("BLsexDiffRejectPassing.mat");
 dataForFemale = loadFile.featureForEachSubjectId{1};
 dataForMale = loadFile.featureForEachSubjectId{2};
 
@@ -23,22 +25,32 @@ WithinDesign = table([1 2 3 4]','VariableNames', {'Concentration'});
 WithinDesign.Concentration = categorical(WithinDesign.Concentration);
 rm = fitrm(t, 'c1-c4 ~ Gender', 'WithinDesign', WithinDesign);
 result = ranova(rm,"WithinModel",'Concentration');
-disp(result);
+
+% disp(result);
+fprintf("Effect of concentration: d.f. = %d, F = %0.4f, p = %0.4e.\n", result.DF(4), ...
+    result.F(4), result.pValue(4));
+fprintf("Effect of gender: d.f. = %d, F = %0.4f, p = %0.4f.\n", result.DF(2), ...
+    result.F(2), result.pValue(2));
+
+% Perform the Kolmogorov-Smirnov test For Gender distribution
+[h2,p2,ks2stat] = kstest2([dataForFemale{:}],[dataForMale{:}]);
+fprintf(['kstest2 results: h=%d, p=%.4e, ks2stat=%.4f ' ...
+    '(overall gender difference)\n'], h2, p2, ks2stat);
 
 % do post hoc multiple comparisons (check m for significant pairwise differences)
 m1 = multcompare(rm, 'Concentration', 'By', 'Gender');
 m2 = multcompare(rm, 'Gender', 'By', 'Concentration');
-for i = 1:2:8
-    fprintf('%.4e\n',m2.pValue(i))
-end
-fprintf('\n');
 
-% Perform the Kolmogorov-Smirnov test For Gender distribution
-[h2,p2,ks2stat] = kstest2([dataForFemale{:}],[dataForMale{:}]);
-fprintf('kstest2 results: h=%d, p=%.4e, ks2stat=%.4f\n', h2, p2, ks2stat);
+fprintf('\nPost-hoc analysis: \n');
+for i = 1:2:8
+    fprintf('%.4f, ',m2.pValue(i))
+end
 fprintf('\n\n');
 
+
 %% perform the KS2 and ranksum test at each conc level
+fprintf(['KStest2 and Wilcoxon rank sum test Results ' ...
+    '(complementary to post-hoc analysis)\n']);
 for i = 1:4 % loop over the four columns c1-c4
     [h, p, ks2stat] = kstest2(dataForFemale{i}, dataForMale{i});
     fprintf('KStest2: Conc %d: h=%d, p=%.4f, ks2stat=%.4f\n', i, h, p, ks2stat);
